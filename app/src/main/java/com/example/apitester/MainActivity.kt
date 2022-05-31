96ee0444d65e765a80dfb6ead6e4f66b0d393e39
Ahmed Khattab <akhattab451@gmail.com>
2022-06-01 00:03:29 +0200

    Added Adapter for Parameters Lists

--- app/src/main/java/com/example/apitester/MainActivity.kt
+++ app/src/main/java/com/example/apitester/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.apitester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.request_type_spinner)
+        spinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_items,
+            android.R.layout.simple_spinner_dropdown_item,
+        )
+
+        val headers: RecyclerView = findViewById(R.id.headers_list)
+        headers.adapter = ParamAdapter()
+        val params: RecyclerView = findViewById(R.id.param_list)
+        params.adapter = ParamAdapter()
+        val body: RecyclerView = findViewById(R.id.body_list)
+        body.adapter = ParamAdapter()
+
+
     }
 }
--- app/src/main/java/com/example/apitester/ParamAdapter.kt
+++ app/src/main/java/com/example/apitester/ParamAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.apitester
 
-class ParamAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+
+class ParamAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val params = MutableList(1) { Pair("", "") }
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    class FooterViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == 0) ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.list_item_param,
+                parent,
+                false
+            )
+        ) else FooterViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.footer_param_list,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is FooterViewHolder) {
+            (holder.itemView as Button).setOnClickListener {
+                params.add(Pair("", ""))
+                notifyItemChanged(position)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = params.size + 1
+
+    override fun getItemViewType(position: Int): Int = if (position == itemCount - 1) 1 else 0
+
+    override fun getItemId(position: Int): Long = position.toLong()
 }
